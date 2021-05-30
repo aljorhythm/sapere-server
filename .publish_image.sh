6e3dc440299ce782be21e92d496600c49a50ace3
Joel Lim <103879u@gmail.com>
2021-05-30 09:01:27 +0800

    edits

--- .publish_image.sh
+++ .publish_image.sh
@@ -1,5 +1,8 @@
 v=$(git rev-parse --short HEAD)
+branch=$(git rev-parse --abbrev-ref HEAD)
 tag=$ENV-$v
+branchtag=$branch-latest
 
 docker push aljorhythm/sapere-server:$tag
-# docker image tag aljorhythm/sapere-server:$tag docker.io/aljorhythm/sapere-server:latest
+docker image tag aljorhythm/sapere-server:$tag docker.io/aljorhythm/sapere-server:$envtag
+docker image tag aljorhythm/sapere-server:$tag docker.io/aljorhythm/sapere-server:$branchtag
