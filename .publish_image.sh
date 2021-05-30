7e421ee0becf5d36d025f4aff979e0f3fcb6f14a
Joel Lim <103879u@gmail.com>
2021-05-30 21:14:22 +0800

    docker

--- .publish_image.sh
+++ .publish_image.sh
@@ -4,9 +4,11 @@ tag=$CI_PLAT.$v
 branch_latest_tag=$(echo $CI_PLAT.$branch.latest | sed 's/\//-/g')
 branch_commit_tag=$(echo $CI_PLAT.$branch.$v | sed 's/\//-/g')
 
+docker pull aljorhythm/sapere-server:$tag
+docker push aljorhythm/sapere-server:$tag
+
 docker tag aljorhythm/sapere-server:$tag docker.io/aljorhythm/sapere-server:$branch_latest_tag
 docker tag aljorhythm/sapere-server:$tag docker.io/aljorhythm/sapere-server:$branch_commit_tag
 
-docker push aljorhythm/sapere-server:$tag
 docker push aljorhythm/sapere-server:$branch_latest_tag
 docker push aljorhythm/sapere-server:$branch_commit_tag
