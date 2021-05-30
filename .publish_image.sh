0336276f61619211fd1ad5422cafb64e031b7a35
Joel Lim <103879u@gmail.com>
2021-05-30 20:10:25 +0800

    correct

--- .publish_image.sh
+++ .publish_image.sh
@@ -3,6 +3,6 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 ci_tag=$CI_PLAT-$v
 branchtag=$(echo $branch-latest | sed 's/\//-/g')
 
-docker push aljorhythm/sapere-server:$tag
+docker push aljorhythm/sapere-server:$ci_tag
 docker image tag aljorhythm/sapere-server:$tag docker.io/aljorhythm/sapere-server:$ci_tag
 docker image tag aljorhythm/sapere-server:$tag docker.io/aljorhythm/sapere-server:$branchtag
